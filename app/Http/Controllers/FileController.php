fe3ea0c0e0801861f0a2cb49e918562f7ab2873e
YulioAdePranata <yulioade0712@gmail.com>
2022-08-05 08:36:05 +0700

    update

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class FileController extends Controller
 {
     public function index(){
-        
+        return view('homeFile', [
+            'tittle' => 'homeFile'
+        ]);
     }
 }
--- resources/views/homeFile.blade.php
+++ resources/views/homeFile.blade.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <nav>
+        <div class="logo">
+            <a href="#">
+                <img src="/Group 3.png" style="width=auto">
+            </a>
+        </div>
+    </nav>
 </head>
 <body>
     
